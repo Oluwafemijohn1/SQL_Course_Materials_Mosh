4ce520811aefe38fb84b87104e3b431c5b75f1f0
Oluwafemi Ogundipe <52871023+Oluwafemijohn@users.noreply.github.com>
2022-10-27 00:55:50 +0100

    essential functions in sql

---
+++ essential_mysql_functions.sql
@@ -0,0 +1,129 @@
+
+-- Numeric Functions
+
+-- SELECT ROUND(5.73) -- 6
+-- SELECT ROUND(5.73, 1) -- 5.7
+-- SELECT ROUND(5.7345, 2) -- 5.73
+-- SELECT ROUND(5.7355, 2) -- 5.74 , this will round off 
+
+-- SELECT TRUNCATE(5.7355, 2) -- 5.73, this will not round off
+
+-- SELECT CEILING(5.7) -- 6 
+-- SELECT CEILING(5.01) -- 6
+-- SELECT CEILING(5.0) -- 5
+
+-- SELECT FLOOR(5.7) -- 5
+-- SELECT FLOOR(5.01); -- 5
+-- SELECT FLOOR(5.0) -- 5
+
+-- SELECT ABS(5.2); -- 5.2
+-- SELECT ABS(-5.2) -- 5.2
+
+-- SELECT RAND() random numbers
+
+
+
+
+
+
+
+
+-- <<<<<String funtions>>>>
+-- SELECT LENGTH('sky');
+-- SELECT UPPER('Hello');
+-- SELECT LOWER('Hello');
+-- SELECT LTRIM('  hello');
+-- SELECT RTRIM('hello  ');
+-- SELECT TRIM('  hello  ');
+-- SELECT LEFT('kindergarteen', 4);
+-- SELECT RIGHT('kindergarteen', 6) -- arteen
+-- SELECT SUBSTRING('kindergarteen', 3, 5) -- nderg
+-- SELECT LOCATE('n', 'kindergarteen') -- 3 position of the first letter
+-- SELECT REPLACE('kindergarteen', 'garteen', 'garden'); 
+-- SELECT CONCAT('first','last');
+
+
+-- USE sql_store;
+-- SELECT CONCAT(first_name, " ", last_name) AS full_name FROM customers
+
+
+
+
+
+
+
+-- Date funtions 
+-- SELECT NOW(), CURDATE(), CURTIME();
+-- SELECT YEAR(NOW()) -- returns year 
+-- SELECT MONTH(NOW()) -- returns month
+-- SELECT HOUR(NOW());
+-- SELECT DAY(NOW());
+-- SELECT MINUTE(NOW());
+-- SELECT SECOND(NOW());
+-- SELECT DAYNAME(NOW())
+-- SELECT MONTHNAME(NOW());
+-- SELECT EXTRACT(DAY FROM NOW());
+-- SELECT YEARWEEK(NOW());
+-- SELECT WEEKOFYEAR(NOW())
+
+
+
+-- From the previous exercises 
+-- SELECT * FROM orders WHERE YEAR(order_date) >= YEAR(NOW())
+
+
+
+-- Formating Dates and Times
+-- SELECT DATE_FORMAT(NOW(), '%y'); -- 22 as year
+-- SELECT DATE_FORMAT(NOW(), '%Y'); -- 2022
+-- SELECT DATE_FORMAT(NOW(), '%m %Y'); -- 10 2022
+-- SELECT DATE_FORMAT(NOW(), '%M %Y'); -- October 2022
+-- SELECT DATE_FORMAT(NOW(), '%M %d %Y'); -- October 26 2022
+-- SELECT DATE_FORMAT(NOW(), '%M %D %Y'); -- October 26th 2022
+-- SELECT DATE_FORMAT(NOW(), '%m-%d-%Y');  -- 10-26-2022
+
+
+-- SELECT TIME_FORMAT(NOW(), '%h:%i'); -- 08:22
+-- SELECT TIME_FORMAT(NOW(), '%H:%i'); -- 20:22
+-- SELECT TIME_FORMAT(NOW(), '%H:%i %p'); -- 20:24 PM
+
+
+-- Calculating date and time 
+-- SELECT DATE_ADD(NOW(), INTERVAL 1 DAY) -- 2022-10-27 21:36:43 added 1 day to current date time
+ -- SELECT DATE_ADD(NOW(), INTERVAL 1 YEAR) -- 2023-10-26 21:37:49
+ -- SELECT DATE_ADD(NOW(), INTERVAL -1 YEAR) -- 2021-10-26 21:38:43 removed one year from current date time  
+ -- Or
+-- SELECT DATE_SUB(NOW(), INTERVAL 1 YEAR) -- 2021-10-26 21:40:01 same as the above 
+
+
+-- Date diff 
+-- SELECT DATEDIFF('2019-01-05', '2019-01-01') -- 4 days, it only returns values in days. also, the later day first
+-- SELECT DATEDIFF('2019-01-05 09:00', '2019-01-01 17:00'); -- 4 days
+-- SELECT TIME_TO_SEC('09:02') - TIME_TO_SEC('09:00') 
+
+
+
+
+-- The IFNULL and COALESCE Functions
+-- USE sql_store;
+-- SELECT 
+-- 	order_id, 
+--     IFNULL(shipper_id, 'Not Assigned') AS shipper
+--     FROM orders
+
+
+-- USE sql_store;
+-- SELECT 
+-- 	order_id, 
+--     COALESCE(shipper_id, comments, 'Not Assigned') AS shipper
+--     FROM orders
+
+
+-- Exercise
+SELECT 
+	CONCAT(first_name, " ", last_name) AS full_name,
+    IFNULL(phone, 'Unknown') AS phone
+    FROM customers
+    
+
+
--- writing_complex_query.sql
+++ writing_complex_query.sql
@@ -1,1 +1,188 @@
 -- A Sub query is a SELECT statement in another sql statement 
+
+-- USE sql_store;
+-- SELECT * FROM products WHERE unit_price > (
+-- 	SELECT unit_price FROM products WHERE product_id =3
+-- )
+
+-- Exercise 
+-- USE sql_hr;
+-- SELECT * FROM employees WHERE salary > (
+-- 	SELECT AVG(salary) FROM employees 
+-- )
+
+
+
+-- The IN operator
+-- USE sql_store;
+-- SELECT * FROM products WHERE product_id NOT IN (
+-- 	SELECT DISTINCT product_id FROM order_items
+-- )
+
+
+-- Exercise 
+-- USE sql_invoicing;
+-- SELECT * FROM clients WHERE client_id NOT IN (
+-- 	SELECT DISTINCT client_id FROM invoices
+--     );
+
+
+-- USE sql_invoicing;
+-- SELECT * FROM clients WHERE client_id NOT IN (
+-- 	SELECT DISTINCT client_id FROM invoices
+--     );
+
+
+-- SELECT * FROM clients 
+-- LEFT JOIN invoices USING (client_id)
+-- WHERE invoice_id IS NULL
+
+
+-- Exercise using sub query
+-- USE sql_store; 
+-- SELECT * FROM customers 
+-- WHERE customer_id IN (
+-- 	SElECT o.customer_id FROM 
+--     order_items oi
+--     JOIN orders o USING (order_id)
+--     WHERE product_id = 3
+-- );
+
+
+-- Exercise using JOIN
+-- USE sql_store; 
+-- SELECT DISTINCT c.customer_id, c.first_name, c.last_name  FROM customers c
+-- JOIN orders o USING (customer_id)
+-- JOIN order_items oi USING (order_id)
+-- WHERE oi.product_id = 3;
+
+
+-- The ALL keyword example
+-- Select invoices larger than all invoices of client 3
+-- USE sql_invoicing;
+-- SELECT * FROM invoices WHERE invoice_total > (
+-- 	SELECT MAX(invoice_total) FROM invoices WHERE client_id = 3
+-- )
+
+
+-- The ALL keyword 
+-- USE sql_invoicing;
+-- SELECT * FROM invoices WHERE invoice_total > ALL (
+-- 	SELECT invoice_total FROM invoices WHERE client_id = 3
+-- );
+
+
+-- ANY keyword
+-- USE sql_invoicing;
+-- SELECT * FROM clients 
+-- 	WHERE client_id IN (
+-- 		SELECT client_id
+-- 		FROM invoices 
+-- 		GROUP BY client_id 
+-- 		HAVING COUNT(*) >= 2
+--     );
+    
+-- Using ANY keyword 
+-- USE sql_invoicing;
+-- SELECT * FROM clients 
+-- 	WHERE client_id = ANY (
+-- 		SELECT client_id
+-- 		FROM invoices 
+-- 		GROUP BY client_id 
+-- 		HAVING COUNT(*) >= 2
+--     );
+
+
+
+-- Correlated Subqueries ( becuase in the subquery we are making reference to the alias from outer query )
+-- The sub query get executed for each row in the query
+-- This can sometimes be slow. The more data we have, the more overhead your query is going to have.
+
+-- USE sql_hr;
+-- SELECT * FROM employees e WHERE salary > (
+-- 	SELECT AVG(salary) FROM employees WHERE office_id = e.office_id
+-- )
+
+
+-- Exercise
+-- USE sql_invoicing;
+-- SELECT * FROM invoices i
+-- WHERE invoice_total > (
+-- 	SELECT AVG(invoice_total)
+--     FROM invoices WHERE client_id = i.client_id
+-- )
+
+
+
+-- Prob:  SELECT client that have invoices 
+-- Exists OPERATOR
+
+-- 1. other ways to solve the same challenge
+-- USE sql_invoicing;
+-- SELECT * FROM clients WHERE client_id IN (
+-- 	SELECT DISTINCT client_id FROM invoices 
+-- );
+
+-- 2. using EXISTS
+-- USE sql_invoicing;
+-- SELECT * FROM clients c
+-- WHERE EXISTS (
+-- 	SELECT client_id
+--     FROM invoices 
+--     WHERE client_id = c.client_id
+-- );
+
+-- Exercise
+-- USE sql_store;
+-- SELECT * FROM products p
+-- WHERE NOT EXISTS (
+-- 	SELECT product_id
+--     FROM order_items
+--     WHERE product_id = p.product_id
+-- );
+
+
+-- Sub queries in the SELECT clause
+-- USE sql_invoicing; 
+-- SELECT invoice_id, invoice_total,
+-- 		(SELECT AVG(invoice_total) FROM invoices ) AS invoice_average,
+--         invoice_total - (SELECT invoice_average) differrence 
+-- 	FROM invoices;
+
+
+-- Exercise
+-- USE sql_invoicing;
+-- SELECT 
+-- 	client_id,
+--     name,
+--     (SELECT SUM(invoice_total) FROM invoices i WHERE c.client_id = i.client_id  ) AS total_sales,
+--     (SELECT AVG(invoice_total) FROM invoices ) AS average,
+--     -- (SELECT total_sales) - (SELECT average) AS difference
+--         (SELECT total_sales - average) AS difference
+--   FROM clients c;
+
+
+-- Using subqueries in FROM clause
+-- USE sql_invoicing;
+-- SELECT * FROM (
+-- 	SELECT 
+-- 		client_id,
+-- 		name,
+-- 		(SELECT SUM(invoice_total) FROM invoices i WHERE c.client_id = i.client_id  ) AS total_sales,
+-- 		(SELECT AVG(invoice_total) FROM invoices ) AS average,
+-- 		(SELECT total_sales - average) AS difference
+-- 	  FROM clients c
+-- ) AS sales_summary;
+
+
+-- Using subqueries in FROM clause and filtering it 
+USE sql_invoicing;
+SELECT * FROM (
+	SELECT 
+		client_id,
+		name,
+		(SELECT SUM(invoice_total) FROM invoices i WHERE c.client_id = i.client_id  ) AS total_sales,
+		(SELECT AVG(invoice_total) FROM invoices ) AS average,
+		(SELECT total_sales - average) AS difference
+	  FROM clients c
+) AS sales_summary WHERE total_sales IS NOT NULL;
