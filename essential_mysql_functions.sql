2444614b805e55ca1b7f515e8711d0f9de3dfeb1
Oluwafemijohn <femiogundipe01@gmail.com>
2022-12-11 20:18:27 +0100

    Learning view

--- essential_mysql_functions.sql
+++ essential_mysql_functions.sql
@@ -164,18 +164,19 @@
 -- 	END AS category
 -- FROM orders
 
+
 -- EXERCISE
-USE sql_store;
-SELECT 
-	CONCAT(first_name, " ", last_name) AS full_name,
-    points,
-    CASE
-		WHEN points > 3000 THEN 'GOLD'
-        WHEN points > 2000 THEN 'SILVER'
-        ELSE 'BRONZE'
-	END AS category
-FROM customers
-ORDER BY points DESC
+-- USE sql_store;
+-- SELECT 
+-- 	CONCAT(first_name, " ", last_name) AS full_name,
+--     points,
+--     CASE
+-- 		WHEN points > 3000 THEN 'GOLD'
+--         WHEN points > 2000 THEN 'SILVER'
+--         ELSE 'BRONZE'
+-- 	END AS category
+-- FROM customers
+-- ORDER BY points DESC
 
 
     
---
+++ view.sql
@@ -0,0 +1,44 @@
+
+/*
+Creating Viewa
+*/
+
+-- USE sql_invoicing;
+-- CREATE VIEW sales_by_client AS
+-- SELECT 
+-- 	c.client_id,
+--     c.name,
+--     SUM(invoice_total) AS total_sales
+-- FROM clients c 
+-- JOIN invoices i USING(client_id)
+-- GROUP BY client_id, name;
+
+
+/*
+Working with view
+*/
+-- Selecting and ordering
+-- SELECT * 
+-- FROM sales_by_client 
+-- ORDER BY total_sales
+-- DESC;
+
+-- Filtering view
+-- SELECT * 
+-- FROM sales_by_client 
+-- WHERE total_sales > 500
+
+
+-- Joining View with other Tables
+-- SELECT * 
+-- FROM sales_by_client 
+-- JOIN clients USING(client_id);
+
+-- 
+SELECT 
+	c.client_id,
+    c.name,
+    SUM(invoice_total - payment_total) AS balance
+FROM clients c
+JOIN invoices i USING(client_id);
+
