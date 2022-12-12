8dc54a6c9a956532d28bb43676af192cbf363534
Oluwafemijohn <femiogundipe01@gmail.com>
2022-12-13 00:28:13 +0100

    Learning views

--- view.sql
+++ view.sql
@@ -34,11 +34,12 @@ Working with view
 -- FROM sales_by_client 
 -- JOIN clients USING(client_id);
 
--- 
+-- Exercise
+CREATE VIEW clients_balance AS 
 SELECT 
 	c.client_id,
     c.name,
     SUM(invoice_total - payment_total) AS balance
 FROM clients c
-JOIN invoices i USING(client_id);
-
+JOIN invoices i USING(client_id)
+GROUP BY client_id, name;
